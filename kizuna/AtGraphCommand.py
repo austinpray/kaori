2730da1115cc2b61cce3857a30e944fc04d2e2f1
Austin Pray <austin@austinpray.com>
2017-11-29 12:35:19 -0600

    Ability to use other layout engines

--- kizuna/AtGraphCommand.py
+++ kizuna/AtGraphCommand.py
@@ -6,15 +6,18 @@ import pygraphviz as pgv
 
 class AtGraphCommand(Command):
     def __init__(self, db_session) -> None:
-        help_text = "{bot} at graph - show the graph of @'s\n"
+        help_text = "{bot} mentions <layout=dot> - show the mention graph. Available formats are" \
+                    "dot, neato, fdp, sfdp, twopi, circo\n "
         self.db_session = db_session
 
-        super().__init__('at-graph', "(?:show )?at graph(?: show)?$", help_text, True)
+        pattern = "mentions(?: (dot|neato|fdp|sfdp|twopi|circo))?$"
+        super().__init__('mention-graph', pattern, help_text, True)
 
     def respond(self, slack_client, message, matches):
         session = self.db_session()
 
         edges = session.query(AtGraphEdge).all()
+        layout = 'dot' if not matches[0] else matches[0]
 
         if not edges or len(edges) < 1:
             return
@@ -24,7 +27,40 @@ class AtGraphCommand(Command):
             G.add_edge(edge.head_user.name, edge.tail_user.name, label=edge.weight, weight=edge.weight)
 
         image_path = '/tmp/graph.png'
-        G.layout(prog='dot')
+
+        def dot():
+            G.layout(prog='dot')
+
+        def neato():
+            G.graph_attr.update(overlap='scale')
+            G.graph_attr.update(splines=True)
+            G.graph_attr.update(sep=1)
+            G.layout(prog='neato')
+
+        def fdp():
+            G.layout(prog='fdp')
+
+        def sfdp():
+            G.layout(prog='sfdp')
+
+        def twopi():
+            G.layout(prog='twopi')
+
+        def circo():
+            G.layout(prog='circo')
+
+        def layout_graph(layout):
+            return {
+                "dot": dot,
+                "neato": neato,
+                "fdp": fdp,
+                "sfdp": sfdp,
+                "twopi": twopi,
+                "circo": circo
+            }.get(layout, dot)
+
+        layout_graph(layout)()
+
         G.draw(image_path)
         slack_client.api_call('files.upload',
                               as_user=True,
