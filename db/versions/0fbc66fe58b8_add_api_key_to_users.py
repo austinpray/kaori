2b06a99f6d796103135a4a37c5656f205c05cbc7
Austin Pray <austin@austinpray.com>
2017-12-03 17:11:50 -0600

    Adds api_key to users

---
+++ db/versions/0fbc66fe58b8_add_api_key_to_users.py
@@ -0,0 +1,39 @@
+"""add api key to users
+
+Revision ID: 0fbc66fe58b8
+Revises: 242a3ab25006
+Create Date: 2017-12-03 22:49:06.036773
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql import select
+
+
+from kizuna.models.User import user_generate_api_key
+
+# revision identifiers, used by Alembic.
+revision = '0fbc66fe58b8'
+down_revision = '242a3ab25006'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('users', sa.Column('api_key', sa.String()))
+    conn = op.get_bind().connect()
+    users = sa.sql.table('users',
+                         sa.Column('id', sa.Integer),
+                         sa.Column('api_key', sa.String))
+
+    users_res = conn.execute(select([users.c.id])).fetchall()
+
+    for user in users_res:
+        conn.execute(users.update().where(users.c.id == user[users.c.id]).values(api_key=user_generate_api_key()))
+
+    op.alter_column('users', 'api_key', nullable=False)
+    op.create_index('ix_api_key', 'users', ['api_key'], unique=True)
+
+
+def downgrade():
+    op.drop_column('users', 'api_key')
--- kizuna/models/User.py
+++ kizuna/models/User.py
@@ -1,13 +1,24 @@
 from sqlalchemy import Column, Integer, String
 from kizuna.models.Models import Base
+from secrets import token_hex
+
+
+def user_generate_api_key():
+    token = token_hex(32)
+    return 'kiz-{}'.format(token)
 
 
 class User(Base):
+    @staticmethod
+    def generate_api_key():
+        user_generate_api_key()
+
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     slack_id = Column(String, index=True, unique=True)
+    api_key = Column(String(), unique=True, index=True, nullable=False, default=user_generate_api_key)
 
     def __repr__(self):
         return "<User(id='{}', name='{}', slack_id='{}')>".format(self.id, self.name, self.slack_id)
