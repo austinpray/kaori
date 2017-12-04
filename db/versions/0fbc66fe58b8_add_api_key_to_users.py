7ebaec5cf58456af225465c999601cdfabac3bd4
Austin Pray <austin@austinpray.com>
2017-12-04 00:26:29 -0600

    Add api_key to users

--- db/versions/0fbc66fe58b8_add_api_key_to_users.py
+++ db/versions/0fbc66fe58b8_add_api_key_to_users.py
@@ -7,7 +7,10 @@ Create Date: 2017-12-03 22:49:06.036773
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.sql import select
 
+
+from kizuna.models.User import user_generate_api_key
 
 # revision identifiers, used by Alembic.
 revision = '0fbc66fe58b8'
@@ -17,8 +20,19 @@ depends_on = None
 
 
 def upgrade():
-    op.add_column('users',
-                  sa.Column('api_key', sa.String(), unique=True, index=True, nullable=False))
+    op.add_column('users', sa.Column('api_key', sa.String()))
+    conn = op.get_bind().connect()
+    users = sa.sql.table('users',
+                         sa.Column('id', sa.Integer),
+                         sa.Column('api_key', sa.String))
+
+    users_res = conn.execute(select([users.c.id])).fetchall()
+
+    for user in users_res:
+        conn.execute(users.update().where(users.c.id == user[users.c.id]).values(api_key=user_generate_api_key()))
+
+    op.alter_column('users', 'api_key', nullable=False)
+    op.create_index('ix_api_key', 'users', ['api_key'], unique=True)
 
 
 def downgrade():
