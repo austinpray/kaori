9c05b13f4eb08a32d1d21a75de4edda179e693ab
Austin Pray <austin@austinpray.com>
2017-12-09 11:14:08 -0600

    pep8

--- kizuna_web/utils.py
+++ kizuna_web/utils.py
@@ -14,4 +14,3 @@ def image_path_to_content_type(path):
         raise ValueError('image type "{}" is not recognized'.format(ext))
 
     return content_type_map[ext]
-
--- scripts/2017-12-set-correct-s3-content-type.py
+++ scripts/2017-12-set-correct-s3-content-type.py
@@ -6,7 +6,7 @@ res = aws_client.list_objects_v2(Bucket=config.S3_BUCKET)
 
 for image in res['Contents']:
     key = image['Key']
-    content_type = image_path_to_content_type(key) # get correct content type for image
+    content_type = image_path_to_content_type(key)  # get correct content type for image
     storage_class = 'REDUCED_REDUNDANCY'  # go ahead and set images as reduced redundancy for cheaper storage
     aws_client.copy_object(Bucket=config.S3_BUCKET,
                            CopySource="{}/{}".format(config.S3_BUCKET, key),
