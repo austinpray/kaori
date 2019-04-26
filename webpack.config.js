b3d329230c27c0c56efca0251442211403e03685
Ridwan Hoq <ridwanhoq@gmail.com>
2019-04-25 21:10:44 -0500

    prettify

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,36 @@
-const path = require('path');
-const webpack = require("webpack");
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-    entry: [
-        'whatwg-fetch',
-        './src/kizuna/web/js/index.js'
+  entry: ['whatwg-fetch', './src/kizuna/web/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'static/dist'),
+    filename: 'kizuna.bundle.js',
+    publicPath: '/static/dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
     ],
-    output: {
-        path: path.resolve(__dirname, 'static/dist'),
-        filename: 'kizuna.bundle.js',
-        publicPath: "/static/dist/"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: "style-loader" },
-                    { loader: "css-loader" }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-            }
-        }),
-    ]
-};
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+  ],
+}
