49fe8591b6ae0d4e829c168f5779ab7b39fc9de2
Austin Pray <austin@austinpray.com>
2020-05-03 13:05:45 -0500

    Enhanced combat sim, ability to run lots of sims

--- kaori/plugins/gacha/engine/test/battle_simulator.py
+++ kaori/plugins/gacha/engine/test/battle_simulator.py
@@ -1,16 +1,44 @@
-from random import random
+import copy
+from random import random, choice
+from typing import List, Optional
 
-from . import meme_cards
+from . import interesting_cards as card_pool
 from .. import *
+
+
+def find_card(module, search: str) -> Card:
+    card_variables: List[Card] = [
+        getattr(module, var_name) for var_name in
+        dir(module) if isinstance(getattr(module, var_name), Card)
+    ]
+    available_cards = {card.slug: card for card in card_variables}
+    if search.lower() == 'random':
+        return choice(list(available_cards.values()))
+
+    return available_cards[Card.sluggify_name(search)]
+
+
+class BattleResult:
+
+    def __init__(self,
+                 winner: Card,
+                 loser: Card,
+                 turns: int) -> None:
+        self.turns = turns
+        self.loser = loser
+        self.winner = winner
 
 
 def run_battle_simulator(card_a,
                          card_b,
                          debug: bool = False,
                          interactive: bool = False,
-                         print_header: bool = True):
-    card_a: Card = getattr(meme_cards, card_a)
-    card_b: Card = getattr(meme_cards, card_b)
+                         print_header: bool = True) -> Optional[BattleResult]:
+    card_a: Card = find_card(card_pool, card_a)
+    card_b: Card = find_card(card_pool, card_b)
+
+    for card in [card_a, card_b]:
+        card.is_valid_card()
 
     print()
 
@@ -23,14 +51,22 @@ def run_battle_simulator(card_a,
         print(card_a.to_markdown())
         print(card_b.to_markdown())
 
+    if Card.detect_standoff(card_a, card_b, debug=False):
+        print(f"These two cards could go at it all day and not harm eachother due to armor, dmg, etc.")
+        print(f"They agree to be friends instead of fighting.")
+        return None
+
     cards = sorted([card_a, card_b], key=lambda card: -card.speed)
     first, second = cards
 
     print(f"{first.name} goes first because of higher speed. {first.speed} vs {second.speed}")
 
+    winner = None
+    loser = None
     turn = 0
+
     while card_a.current_hp > 0 and card_b.current_hp > 0:
-        if turn > 1000:
+        if turn > 20:
             raise RuntimeError('This battle is taking too long...')
 
         current_card = cards[turn % len(cards)]
@@ -38,27 +74,33 @@ def run_battle_simulator(card_a,
         print(f"{current_card.name} is attacking")
         # evasion check
         if random() < target_card.evasion:
-            print(f"{target_card.name} dodged the attack")
+            print(f"**{target_card.name} dodged the attack**")
             turn += 1
             continue
 
         crit_multiplier = 1
         # crit
         if random() < current_card.crit:
-            crit_multiplier = 3
-            print(f"{current_card.name} hit a crit!")
+            crit_multiplier = CRIT_MULTIPLIER
+            print(f"**{current_card.name} hit a crit!**")
 
-        damage = current_card.attack_damage(target_card, crit_multiplier=crit_multiplier)
+        damage = current_card.attack_damage(target_card, crit_multiplier=crit_multiplier, debug=debug)
         print(f"{current_card.name} will hit {target_card.name} for {damage}!")
         target_card.accept_damage(damage)
 
         if target_card.current_hp < 1:
             print(f"{target_card.name} was killed!!")
+            winner = current_card
+            loser = target_card
+            break
         else:
             print(f"{target_card.name} has {target_card.current_hp} HP left")
 
         turn += 1
 
     print("battle over")
+    br = BattleResult(winner=copy.deepcopy(winner), loser=copy.deepcopy(loser), turns=turn + 1)
     for card in cards:
         card.reset_hp()
+    return br
+
