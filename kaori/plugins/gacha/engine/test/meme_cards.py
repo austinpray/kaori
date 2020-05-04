724ba6fae7f9a48d594455b30a17329fcd652489
Austin Pray <austin@austinpray.com>
2020-05-03 21:21:55 -0500

    Add rust + hicock

--- kaori/plugins/gacha/engine/test/meme_cards.py
+++ kaori/plugins/gacha/engine/test/meme_cards.py
@@ -41,7 +41,6 @@ matt_morgan = Card(name="Matt Morgan",
                    cursed=7,
                    feral=2)
 
-
 tlp = Card(name="TLP",
            rarity=C,
            nature=(horny, feral),
@@ -51,3 +50,25 @@ tlp = Card(name="TLP",
            horny=5,
            cursed=2,
            feral=3)
+
+hicock45 = Card(name="hicock45",
+                rarity=B,
+                nature=(horny, feral),
+                stupid=2,
+                baby=3,
+                clown=3,
+                horny=4,
+                cursed=1,
+                feral=6)
+
+the_rust_programming_language = Card(
+    name="The Rust Programming Language",
+    rarity=B,
+    nature=(horny, feral),
+    stupid=3,
+    baby=2,
+    clown=1,
+    horny=5,
+    cursed=4,
+    feral=4,
+)
