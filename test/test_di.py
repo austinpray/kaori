d92ec5394b6035a30ac4face586e924cce08db96
Austin Pray <austin@austinpray.com>
2020-06-07 08:26:56 -0500

    Allow DI to inject handle subclasses and ABCs (#69)
    * DI for subclasses and ABCs
    
    Add tests for di
    
    * add negative test

---
+++ test/test_di.py
@@ -0,0 +1,55 @@
+from abc import ABC
+
+from kaori.support.di import build_di_args
+
+
+# abc zone
+
+class _Vehicle(ABC):
+    pass
+
+
+class _Car(_Vehicle):
+    pass
+
+
+# reggo classes
+
+class _Cute:
+    pass
+
+
+class _CatGirl(_Cute):
+    pass
+
+
+def _handle_car(car: _Car):
+    pass
+
+
+def _handle_vehicle(car: _Vehicle):
+    pass
+
+
+def _handle_cute(cute: _Cute):
+    pass
+
+
+def _handle_cat_girl(owo: _CatGirl):
+    pass
+
+
+def test_di():
+    car = _Car()
+    cat_girl = _CatGirl()
+    components = {car, cat_girl}
+    explicit = build_di_args(components, _handle_car)
+    assert car in explicit.values()
+    implied = build_di_args(components, _handle_vehicle)
+    assert car in implied.values()
+
+    explicit = build_di_args(components, _handle_cat_girl)
+    assert cat_girl in explicit.values()
+    implied = build_di_args(components, _handle_cute)
+    assert cat_girl in implied.values()
+    assert car not in implied.values()
