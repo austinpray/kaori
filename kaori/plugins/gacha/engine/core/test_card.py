22d156471ffdd9fa5d0b8ef21cad2f2d96da8805
Austin Pray <austin@austinpray.com>
2020-06-14 00:19:23 -0500

    Fixes bug: StatName -> RarityName
    oops

--- kaori/plugins/gacha/engine/core/test_card.py
+++ kaori/plugins/gacha/engine/core/test_card.py
@@ -1,6 +1,23 @@
+from .card import Card
+
+
 def test_card_text():
     from ..test.cards.meme_cards import matt_morgan, ubu, kim_jong_un_lil_sis
 
     assert matt_morgan.title == 'Matt Morgan (S-tier cursed baby)'
     assert ubu.title == 'ubu (C-tier feral and cursed)'
     assert kim_jong_un_lil_sis.title == "Kim Jong Un's Little Sister (S-tier horny baby)"
+
+def test_card_generate():
+    card = Card.generate('ubu2', 'F', ['horny', 'clown'])
+    assert card.title == 'ubu2 (F-tier horny clown)'
+
+    assert sum([
+        card.stupid,
+        card.baby,
+        card.clown,
+        card.horny,
+        card.cursed,
+        card.feral,
+    ]) == 10
+
