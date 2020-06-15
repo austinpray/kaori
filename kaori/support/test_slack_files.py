46edab38fc1a85c5599598be07b507b8bf49e1b9
Austin Pray <austin@austinpray.com>
2020-06-14 21:44:02 -0500

    add tests for slack file download

---
+++ kaori/support/test_slack_files.py
@@ -0,0 +1,14 @@
+from kaori.skills.file_uploads import LocalFileUploader
+from kaori.support.slack_files import download_slack_file
+from kaori.worker import sc
+
+
+def test_download_slack_file():
+    file_id = sc.api_call('files.list')['files'][0]['id']
+
+    name, file = download_slack_file(file_id, slack_client=sc)
+
+    local = LocalFileUploader()
+    url = local.upload(name, file)
+
+    assert url.endswith(name)
