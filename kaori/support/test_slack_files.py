cf5c2d40418f40806a4ad85e350376032df90dca
Austin Pray <austin@austinpray.com>
2022-11-12 18:40:00 +0000

    skip test with remote failure

--- kaori/support/test_slack_files.py
+++ kaori/support/test_slack_files.py
@@ -2,7 +2,9 @@ from kaori.skills.file_uploads import LocalFileUploader
 from kaori.support.slack_files import download_slack_file
 from kaori.worker import sc
 
+import pytest
 
+@pytest.mark.skip(reason="Can break if testbed slack goes unused for too long")
 def test_download_slack_file():
     file_id = sc.api_call('files.list')['files'][0]['id']
 
